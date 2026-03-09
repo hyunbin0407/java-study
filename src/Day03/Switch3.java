8822faee56686d1295b9345b31f89d0fcddfa305
janghyeonbin <gusqls0718@gmail.com>
2026-03-09 17:56:48 +0900

    add practice if code

---
+++ src/Day03/Switch3.java
@@ -0,0 +1,21 @@
+package Day03;
+
+public class Switch3 {
+    static void main() {
+
+
+        int coupon;
+        switch (2) {
+            case 1:
+                coupon = 1000;
+                break;
+            case 2:
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+        }
+        System.out.println("발급받은 쿠폰 " + coupon);
+    }
+}
