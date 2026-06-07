6a49e99af32c00e4153592f123bf9a1ce2e224b2
janghyeonbin <gusqls0718@gmail.com>
2026-06-07 22:09:02 +0900

    pratice

---
+++ src/Day13/PackMain3.java
@@ -0,0 +1,10 @@
+package Day13;
+
+import Day13.a.*;
+
+public class PackMain3 {
+    static void main() {
+        User userA = new User();
+        Day13.b.User userB = new Day13.b.User();
+    }
+}
