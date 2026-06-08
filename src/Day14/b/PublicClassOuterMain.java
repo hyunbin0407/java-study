f48d164260de0fb4ca83df44c85c213263083a80
janghyeonbin <gusqls0718@gmail.com>
2026-06-09 01:03:01 +0900

    pratice

---
+++ src/Day14/b/PublicClassOuterMain.java
@@ -0,0 +1,14 @@
+package Day14.b;
+
+//import access.a.DefaultClass1;
+import Day14.a.PublicClass;
+
+public class PublicClassOuterMain {
+    public static void main(String[] args) {
+        PublicClass publicClass = new PublicClass();
+
+        //다른 패키지 접근 불가
+        //DefaultClass1 class1 = new DefaultClass1();
+        //DefaultClass2 class2 = new DefaultClass2();
+    }
+}
