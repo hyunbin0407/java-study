a4ff3e36d72a3f20d4c19977a4dfcef03bfeca29
janghyeonbin <gusqls0718@gmail.com>
2026-06-16 23:25:46 +0900

    pratice

---
+++ src/Day15/static1/Data1.java
@@ -0,0 +1,11 @@
+package Day15.static1;
+
+public class Data1 {
+    public String name;
+    public int count;
+
+    public Data1(String name) {
+        this.name = name;
+        count++;
+    }
+}
