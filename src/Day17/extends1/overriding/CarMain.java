9f5222c7c6e41111889bd05933f2c31d501e20ca
janghyeonbin <gusqls0718@gmail.com>
2026-06-20 22:30:59 +0900

    pratice

---
+++ src/Day17/extends1/overriding/CarMain.java
@@ -0,0 +1,12 @@
+package Day17.extends1.overriding;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+    }
+}
