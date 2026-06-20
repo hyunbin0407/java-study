703afa355d64017029698c87f35040cbcd75af41
janghyeonbin <gusqls0718@gmail.com>
2026-06-20 22:31:48 +0900

    pratice

---
+++ src/Day17/extends1/super2/Super2Main.java
@@ -0,0 +1,9 @@
+package Day17.extends1.super2;
+
+public class Super2Main {
+
+    public static void main(String[] args) {
+        //ClassC classC = new ClassC();
+        ClassB classB = new ClassB(100);
+    }
+}
