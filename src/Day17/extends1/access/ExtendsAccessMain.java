3a4dc629efc769e66d626bd86aaa461138a7109b
janghyeonbin <gusqls0718@gmail.com>
2026-06-20 22:31:21 +0900

    pratice

---
+++ src/Day17/extends1/access/ExtendsAccessMain.java
@@ -0,0 +1,11 @@
+package Day17.extends1.access;
+
+import Day17.extends1.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
